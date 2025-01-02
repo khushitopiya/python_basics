4ec5f46254f264046c641c516cf761c45d7bf13d
Khushi Topiya <114566494+khushitopiya@users.noreply.github.com>
2025-01-02 18:59:59 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,61 @@
+#list
+marks1 = 99.9
+marks2 = 88.8
+marks3 = 77.7
+
+marks = [99.9,88.8,77.7]
+print(marks)
+
+
+
+marks = [99.9,88.8,77.7, 66.6]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+
+
+movie = []
+mov1 = input("Enter the name of the movie: ")
+mov2 = input("Enter the name of the movie: ")
+mov3 = input("Enter the name of the movie:")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+
+print(movies)
+
+#list append
+list = [2,6,8]
+list.append(4)
+print(list)
+
+#list sort
+list = [2,6,8]
+print(list.sort())
+print(list)
+
+#sort revers
+list = [2,4,6,8,10]
+print(list.sort(reverse=True))
+print(list)
+
+#acsendig order
+list = ['mango', 'kiwi', 'banana', 'apple', 'watermelon']
+print(list.sort(reverse=False))
+print(list)
+
+#list remove
+list = [2,4,6,8,10]
+list.pop(1)
+print(list)
+
+
+#tuple
+tuple =(1,2,3,4,5)
+print(tuple[0])
+
+#index
+indextup = (1,2,3,4)
+print(tup.index(2))
